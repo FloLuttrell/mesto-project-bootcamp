6167648dff6f5ba4be6799a64d074b0137be56b0
Valeriia Kostkina <floluttrell@gmail.com>
2023-11-02 21:41:20 +0300

    add: image popup

--- script.js
+++ script.js
@@ -1,35 +1,108 @@
 "use strict";
 
-let editBtn = document.querySelector(".profile__edit-button");
-editBtn.addEventListener("click", function () {
-  let popup = document.querySelector(".popup");
-  popup.classList.add("popup_opened");
-  let nameEl = document.querySelector(".profile__name");
-  let nameInput = document.querySelector(".form__input_type_user-name");
-  nameInput.value = nameEl.textContent;
-  let aboutEl = document.querySelector(".profile__about");
-  let aboutInput = document.querySelector(".form__input_type_about");
-  aboutInput.value = aboutEl.textContent;
-});
-
-const closePopup = function () {
-  let popup = document.querySelector(".popup");
+const closePopup = function (popup) {
   popup.classList.remove("popup_opened");
 };
 
-let exitBtn = document.querySelector(".popup__close");
-exitBtn.addEventListener("click", closePopup);
+const openPopup = function (popup) {
+  popup.classList.add("popup_opened");
+};
 
-let submitProfile = document.querySelector(".form");
-submitProfile.addEventListener("submit", function (event) {
+const popups = document.querySelectorAll(".popup");
+popups.forEach(function (popup) {
+  const exitBtn = popup.querySelector(".popup__close");
+  exitBtn.addEventListener("click", function () {
+    closePopup(popup);
+  });
+});
+
+const editProfileButton = document.querySelector(".profile__edit-button");
+editProfileButton.addEventListener("click", function () {
+  const popup = document.querySelector(".popup_type_editUser");
+  openPopup(popup);
+  const nameEl = document.querySelector(".profile__name");
+  const nameInput = document.querySelector(".form__input_type_user-name");
+  nameInput.value = nameEl.textContent;
+  const aboutEl = document.querySelector(".profile__about");
+  const aboutInput = document.querySelector(".form__input_type_about");
+  aboutInput.value = aboutEl.textContent;
+});
+
+const profleForm = document.querySelector(".popup_type_editUser .form");
+profleForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  let nameEl = document.querySelector(".profile__name");
-  let nameInput = document.querySelector(".form__input_type_user-name");
+  const nameEl = document.querySelector(".profile__name");
+  const nameInput = document.querySelector(".form__input_type_user-name");
   nameEl.textContent = nameInput.value;
-  let aboutEl = document.querySelector(".profile__about");
-  let aboutInput = document.querySelector(".form__input_type_about");
+  const aboutEl = document.querySelector(".profile__about");
+  const aboutInput = document.querySelector(".form__input_type_about");
   aboutEl.textContent = aboutInput.value;
-  closePopup();
+  const popup = document.querySelector(".popup_type_editUser");
+  closePopup(popup);
+});
+
+const imageForm = document.querySelector(".popup_type_addElement .form");
+imageForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const imageName = document.querySelector(".form__input_type_image-name");
+  const imageUrl = document.querySelector(".form__input_type_image-link");
+  const userCard = {
+    name: imageName.value,
+    link: imageUrl.value,
+  };
+  const newUserCard = createItem(userCard);
+
+  addItem(newUserCard);
+  const popup = document.querySelector(".popup_type_addElement");
+  closePopup(popup);
+  imageName.value = "";
+  imageUrl.value = "";
+});
+
+const cardTemplate = document
+  .querySelector(".card-template")
+  .content.querySelector(".elements__card");
+function createItem(item) {
+  // creating
+  const newItem = cardTemplate.cloneNode(true);
+  const elementName = newItem.querySelector(".elements__title");
+  elementName.textContent = item.name;
+  const elementImage = newItem.querySelector(".elements__image");
+  elementImage.src = item.link;
+  elementImage.alt = item.name;
+  // deleting
+  const deleteCard = newItem.querySelector(".elements__trash-button");
+  deleteCard.addEventListener("click", () => {
+    newItem.remove();
+  });
+  //like
+  const likeCard = newItem.querySelector(".elements__like-button");
+  likeCard.addEventListener("click", () => {
+    likeCard.classList.toggle("elements__like-button_active");
+  });
+  //image
+  const imgBtn = newItem.querySelector(".elements__image");
+  imgBtn.addEventListener("click", () => {
+    const popupImg = document.querySelector(".popup_type_image");
+    const bigImg = popupImg.querySelector(".popup__image");
+    bigImg.src = item.link;
+    const popupName = popupImg.querySelector(".popup__image-title");
+    popupName.textContent = item.name;
+    openPopup(popupImg);
+  });
+
+  return newItem;
+}
+
+const itemsBlock = document.querySelector(".elements__list");
+function addItem(item) {
+  itemsBlock.append(item);
+}
+
+const cardAddButton = document.querySelector(".profile__add-button");
+cardAddButton.addEventListener("click", function () {
+  const popup = document.querySelector(".popup_type_addElement");
+  openPopup(popup);
 });
 
 const initialCards = [
@@ -59,42 +132,7 @@ const initialCards = [
   },
 ];
 
-const cardTemplate = document
-  .querySelector(".card-template")
-  .content.querySelector(".elements__card");
-const itemsBlock = document.querySelector(".elements__list");
-
-const itemForm = document.querySelector(".form");
-
-function createItem(item) {
-  // creating
-  const newItem = cardTemplate.cloneNode(true);
-  const elementName = newItem.querySelector(".elements__title");
-  elementName.textContent = item.name;
-  const elementImage = newItem.querySelector(".elements__image");
-  elementImage.src = item.link;
-  elementImage.alt = item.name;
-  // deleting
-  const deleteCard = newItem.querySelector(".elements__trash-button");
-  deleteCard.addEventListener("click", () => {
-    newItem.remove();
-  });
-
-  return newItem;
-}
-
-function addItem(item) {
-  itemsBlock.append(item);
-}
-
 initialCards.forEach((item) => {
   const newCard = createItem(item);
   addItem(newCard);
-});
-
-let newlike = document.querySelector(".elements__list");
-newlike.addEventListener("click", function (ev) {
-  if (ev.target.classList.contains("elements__like-button")) {
-    ev.target.classList.toggle("elements__like-button_active");
-  }
 });
