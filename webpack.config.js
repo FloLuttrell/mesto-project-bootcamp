1157dfe42d7743e5c84283444b53ab509005714a
Valeriia Kostkina <floluttrell@gmail.com>
2023-11-07 21:31:02 +0300

    add: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -20,11 +20,12 @@ module.exports = {
         port: 8080
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            use: 'babel-loader',
-            exclude: '/node_modules/'
-        },
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
             {
                 test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
                 type: 'asset/resource',
